a0e2fa34efedfc257b2c15e677581e0bf72f0220
Adrian-79 <48460668+Adrian-79@users.noreply.github.com>
2025-06-17 01:27:03 -0300

    Add files via upload

---
+++ views/db.php
@@ -0,0 +1,11 @@
+//Este archivo ahora depende de config.php, que a su vez lee del .env_entorno. se reemplaza
+//todo el contenido de db.php por este otro.
+<?php
+require_once(__DIR__ . '/../config.php');
+
+try {
+    $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $exception) {
+    echo "Connection error: " . $exception->getMessage();
+}
