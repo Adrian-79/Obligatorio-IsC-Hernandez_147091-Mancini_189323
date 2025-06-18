a7148511268fb9357c8029f0f785dd872f2d3fda
Adrian-79 <48460668+Adrian-79@users.noreply.github.com>
2025-06-18 14:31:58 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,2 @@
 <?php
-$dotenv = parse_ini_file(__DIR__ . '/.env');
-define('DB_HOST', $dotenv['DB_HOST']);
-define('DB_NAME', $dotenv['DB_NAME']);
-define('DB_USER', $dotenv['DB_USER']);
-define('DB_PASS', $dotenv['DB_PASS']);
+// Este archivo será sobreescrito automáticamente por el user_data.sh
