5c2d3ef98906568d894192f38e7ab694836fca4c
Adrian-79 <48460668+Adrian-79@users.noreply.github.com>
2025-06-20 02:59:50 -0300

    Update db.php

--- views/db.php
+++ views/db.php
@@ -1,14 +1,11 @@
-<?php 
-
-$host = getenv('DB_HOST');
-$name = getenv('DB_NAME');
-$user = getenv('DB_USER');
-$password = getenv('DB_PASS');
+<?php
+require_once(__DIR__ . '/../config/config.php');
 
 try {
-	$pdo = new PDO("mysql:host=$host;dbname=$name", $user, $password);
-	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $exception) {
-	echo "Connection error :" . $exception->getMessage();
+    $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $exception) {
+    echo "❌ Connection error: " . $exception->getMessage();
 }
+?>
 
